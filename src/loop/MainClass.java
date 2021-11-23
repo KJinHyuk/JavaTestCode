d7d3a873a81befd0d85c4255e20829a1614d57b0
KJinHyuk <wlsgurrn@naver.com>
2021-11-24 07:22:02 +0900

    나무꾼

--- src/loop/MainClass.java
+++ src/loop/MainClass.java
@@ -1,30 +1,29 @@
-<<<<<<< HEAD
 package loop;
-=======
 
->>>>>>> refs/remotes/origin/master
 import java.util.Scanner;			//main 에서 sc9를 사용 하기 위한 import 
+import java.util.ArrayList;
 import java.util.Random;			//main 에서 rand를 사용 하기 위한 import
 
 public class MainClass {
 	public static void main(String[] args) {
 		// 나무를 도끼로 쓰러 뜨려 보자
-		// 쓰러뜨릴 나무의 갯수(최대 10개)를 입력 받고
+		// 쓰러뜨릴 나무의 갯수를 입력 받고
 		// 나무 내구도(최대 10) 만큼 도끼질로 나무를 쓰러뜨리자
 
 		// 나무들의 내구도를 랜덤 수치로 받기 위한 배열 선언
-		int[] StTree = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }; 
+		ArrayList<Integer> StTree = new ArrayList<Integer>();
+		Scanner sc = new Scanner(System.in); 	// Scanner 호출
+		Random rand = new Random();				// Random 호출
+//		int[] StTree = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }; 
 		// InTree = 총 나무 그루 수
 		int InTree;
-		System.out.print("원하는 나무 갯수를 입력하세요(최대 10그루) : ");
-		Scanner sc = new Scanner(System.in); 	// 입력값을 저장
+		System.out.print("원하는 나무 갯수를 입력하세요 : ");
 		InTree = sc.nextInt(); 					// 입력값을 변수에 저장
-		Random rand = new Random();
 		for (int i = 0; i < InTree; i++) {
-			int iValue = rand.nextInt(10) + 1; 	// 1 ~ 10 사이의 값을 임의수를 나무 내구도로 저장
-			StTree[i] = iValue; 			    // 순서대로 임의수를 배열에 저장
+			StTree.add(rand.nextInt(10) + 1); 	// 1 ~ 10 사이의 값을 임의수를 나무 내구도로 저장
+			int iValue = StTree.get(i); 			    // 순서대로 임의수를 리스트에 저장
 			int b = 0;
-			while (b < StTree[i]) {
+			while (b < iValue) {
 				System.out.println((i + 1) + " 번째 나무를 도끼로 " + (b + 1) + "회 찍었다");
 				if (b == iValue) {
 					System.out.println("나무가 넘어간다");
@@ -39,7 +38,7 @@ public class MainClass {
 		
 		// 총 나무 숫자와 각각 나무의 내구도 확인
 		for (int n = 0; n < InTree; n++) {	
-			System.out.println("총 " + InTree + "그루 중에 " + (n + 1) + "번째 나무 내구도는 " + StTree[n] + " 입니다.");
+			System.out.println("총 " + InTree + "그루 중에 " + (n + 1) + "번째 나무 내구도는 " + StTree.get(n) + " 입니다.");
 		}// for
 	}// main
 }// class
